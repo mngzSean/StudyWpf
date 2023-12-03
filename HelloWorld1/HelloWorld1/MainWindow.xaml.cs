d4c00818e6eee86ac454c470d4168bcb3893ad42
mngz <mngz2537@gmail.com>
2023-12-04 02:14:51 +0900

    hello world 간단한 예제 작성
    Label, TextBox, Button, TextBlock 간단하게 살펴보기

---
+++ HelloWorld1/HelloWorld1/MainWindow.xaml.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace HelloWorld1
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+        }
+
+        private void OnButtonClick(object sender, RoutedEventArgs e)
+        {
+            MessageBox.Show(textBox.Text + "님 환영합니다.", "Hello World");
+        }
+
+        private void TextBolck_MouseButtonUp(object sender, MouseButtonEventArgs e)
+        {
+            MessageBox.Show("Hi There!", "Hello world", MessageBoxButton.OK, MessageBoxImage.Information);
+        }
+    }
+}
