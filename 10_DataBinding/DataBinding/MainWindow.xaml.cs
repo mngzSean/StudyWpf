6c435ba9a4cef8a9a669932f190d3567ab795779
mngz <mngz2537@gmail.com>
2023-12-07 02:21:28 +0900

    ViewModel를 만들어 비하인드 코드의 변수와 바인딩하고 속성 값들을 TwoWay 모드로 작업해서 변경되는지 확인

---
+++ 10_DataBinding/DataBinding/MainWindow.xaml.cs
@@ -0,0 +1,56 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace DataBinding
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        // UI 컨트롤에서 바인딩으로 사용할 소스 속성들
+        public bool Seoul {  get; set; }
+        public bool Jejoo { get; set; }
+        public bool Incheon {  get; set; }
+
+        MyViewModel _viewModel;
+
+        public MainWindow()
+        {
+            InitializeComponent();
+            // 바인딩의 소스객체, UI컨트롤에서 별도의 소스 지정없이 사용가능
+            // Window의 하위객체에서 소스 속성으로 사용가능
+            //this.DataContext = this;
+
+            //create a model object
+            _viewModel = new MyViewModel()
+            {
+                Date = new DateTime(2023, 12, 7),
+                Title = "WPF User Group"
+            };
+            this.DataContext = _viewModel;
+        }
+
+        private void Submit_Click(object sender, RoutedEventArgs e)
+        {
+            MessageBox.Show(string.Format("SEOUL: {0},JEJOO: {1}, INCHEON: {2}", Seoul, Jejoo, Incheon));
+        }
+
+        private void Button_Click(object sender, RoutedEventArgs e)
+        {
+            MessageBox.Show(string.Format("{0}\n{1}", _viewModel.Title, _viewModel.Date.ToString()));
+        }
+    }
+}
