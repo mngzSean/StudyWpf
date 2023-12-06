83c88a1ae240644731f08067ce306d51a2fb5c41
mngz <mngz2537@gmail.com>
2023-12-07 05:11:39 +0900

    DataBinding을 할 때 IValueConverter를 사용하여 bool타입과 string타입을 변환하여 사용

---
+++ 18_DataBinding_IValueConverter/DataBinding_IValueConverter/YesNoToBoolConverter.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Globalization;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Data;
+
+namespace DataBinding_IValueConverter
+{
+    class YesNoToBoolConverter : IValueConverter
+    {
+        // 소스 값이 타겟에 바인딩 되는 경우 호출
+        // TextBox --> TextBlock, TextBox --> CheckBox
+        // TextBox의 Text속성의 값 "YES", "NO"에 따라 true, false를 리턴
+        public object Convert(object value, Type targetType, object parameter, CultureInfo culture)
+        {
+            switch(value.ToString()!.ToUpper())
+            {
+                case "YES": return true;
+                case "NO": return false;
+            }
+            return false;
+        }
+
+        // 타켓값이 역으로 소스에 바인딩 될때 호출
+        public object ConvertBack(object value, Type targetType, object parameter, CultureInfo culture)
+        {
+            if(value is not null)
+            {
+                if ((bool)value!)
+                    return "YES";
+            }
+
+            return "NO";
+        }
+    }
+}
