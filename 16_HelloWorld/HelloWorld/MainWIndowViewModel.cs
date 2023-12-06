635fbac4b098d08db4671304ac9143eb98970b96
mngz <mngz2537@gmail.com>
2023-12-07 04:51:14 +0900

    Command패턴 연습

---
+++ 16_HelloWorld/HelloWorld/MainWIndowViewModel.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Input;
+
+namespace HelloWorld
+{
+    class MainWIndowViewModel
+    {
+        //private ICommand? _ButtonCommand;
+        //public ICommand? ButtonCommand
+        //{
+        //    get
+        //    {
+        //        return _ButtonCommand;
+        //    }
+        //    set
+        //    {
+        //        _ButtonCommand = value;
+        //    }
+        //}
+        public ICommand ButtonCommand { get; set; }
+
+        public MainWIndowViewModel()
+        {
+            ButtonCommand = new RelayCommand(param =>
+            {
+                MessageBox.Show("Hi~ " + param.ToString());
+            });
+        }
+    }
+}
---
+++ 16_HelloWorld/HelloWorld/RelayCommand.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Input;
+
+namespace HelloWorld
+{
+    class RelayCommand : ICommand
+    {
+        Action<object> _action;
+
+        public RelayCommand(Action<object> action)
+        {
+            this._action = action;
+        }
+
+        //public event EventHandler? CanExecuteChanged;
+        public event EventHandler? CanExecuteChanged
+        {
+            add { CommandManager.RequerySuggested += value; }
+            remove { CommandManager.RequerySuggested -= value; }
+        }
+
+        public bool CanExecute(object? parameter)
+        {
+            if (parameter == null)
+                return false;
+
+            if(parameter.ToString()!.Length == 0)
+                return false;
+
+            return true;
+        }
+
+        public void Execute(object? parameter)
+        {
+            _action(parameter);
+        }
+    }
+}
