9799cb06b291317e50c73fc035d8080048880f36
mngz <mngz2537@gmail.com>
2023-12-07 22:08:19 +0900

    DataTemplate 샘플 테스트

---
+++ TemplateTest/DataTemplateTest/MainViewModel.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Linq;
+using System.Runtime.CompilerServices;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DataTemplateTest
+{
+    class MainViewModel 
+    {
+        public event PropertyChangedEventHandler? PropertyChanged;
+        public void OnPropertyChanged([CallerMemberName] string? propertyName = null)
+        {
+            PropertyChanged?.Invoke(this, new PropertyChangedEventArgs(propertyName));
+        }
+
+        private List<Student> students = default!;
+        public MainViewModel()
+        {
+            Students = Student.Students;
+        }
+
+        public List<Student> Students 
+        { 
+            get => students; 
+            set
+            { 
+                students = value; 
+                OnPropertyChanged();
+            } 
+        }
+    }
+}
